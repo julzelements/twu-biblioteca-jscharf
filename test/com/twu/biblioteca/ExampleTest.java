6206c4f2b2407f388ac14b37e0d7230551d38d70
Julian Scharf <jscharf@thoughtworks.com>
2016-08-12 15:31:12 +1000

    Successffully tested the first user story.

--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,13 +1,31 @@
 package com.twu.biblioteca;
 
-
 import org.junit.Test;
+
+import java.util.logging.ConsoleHandler;
+
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
 
+
+
+    @Test
+    public void testWelcomePage() throws Exception {
+        BibliotecaApp app = new BibliotecaApp();
+        app.run();
+        ConsoleOutputCapturer capturer = new ConsoleOutputCapturer();
+        capturer.start();
+        app.run();
+        String greeting = capturer.stop();
+
+
+        assertEquals(greeting, "Hello! \nWelcome to Biblioteca.\n\n");
+    }
+
     @Test
     public void test() {
+
         assertEquals(1, 1);
     }
 }
