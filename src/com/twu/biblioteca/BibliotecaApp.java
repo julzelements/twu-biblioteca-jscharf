a0d1a1966657bc1237830f5b1c0ae8a90e49b968
Julian Scharf <jscharf@thoughtworks.com>
2016-08-15 10:43:41 +1000

    Test Commit 2

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -12,14 +12,6 @@ public class BibliotecaApp {
 
     public void welcomeMessage() {
         System.out.println("Hello! \nWelcome to Biblioteca.\n");
-//        String begin = userInput.getString("press any key to begin");
-//
-//        if (begin != null) {
-//            System.out.println("user has begun");
-//
-//        } else {
-//            welcomeMessage();
-//        }
     }
 
 
