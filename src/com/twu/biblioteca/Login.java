ed05e3887140df8c3a099ca92f358741bd37de95
Julian Scharf <jscharf@thoughtworks.com>
2016-09-26 20:58:44 +1000

    Validated some methods for logins

--- src/com/twu/biblioteca/Login.java
+++ src/com/twu/biblioteca/Login.java
@@ -1,10 +1,27 @@
 package com.twu.biblioteca;
 
+import java.util.HashMap;
+
 public class Login {
 
     UserInput userInput;
+    HashMap<String, String> userLogins;
 
     public Login(UserInput userInput) {
         this.userInput = userInput;
+        this.userLogins = new HashMap<String, String>();
+        userLogins.put("Tommy", "password");
+    }
+
+    public boolean userExists(String loginName) {
+        if (userLogins.keySet().contains(loginName)) {
+            return true;
+        }
+    return false;
+    }
+
+    public boolean correctPassword(String name, String password) {
+        if (!userExists(name)) return false; {
+        } return userLogins.get(name).equals(password);
     }
 }
--- test/com/twu/biblioteca/LoginTests.java
+++ test/com/twu/biblioteca/LoginTests.java
@@ -5,17 +5,22 @@ import org.junit.Test;
 
 import static junit.framework.TestCase.assertNotNull;
 import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertFalse;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class LoginTests {
     UserInput mockUserInput;
+    UserInput invalidMockUserInput;
 
     @Before
     public void setUp() throws Exception {
         mockUserInput = mock(UserInput.class);
         when(mockUserInput.getString(anyString())).thenReturn("Tommy").thenReturn("password");
+
+        invalidMockUserInput = mock(UserInput.class);
+        when(invalidMockUserInput.getString(anyString())).thenReturn("BadName").thenReturn("BadPassword");
 
     }
 
@@ -26,10 +31,48 @@ public class LoginTests {
     }
 
     @Test
-    public void testThatMokitoMockReturnsTommyThenPassword() throws Exception {
+    public void testThatValidUserMockReturnsTommyThenPassword() throws Exception {
         Login login = new Login(mockUserInput);
         assertTrue(login.userInput.getString("anything").equals("Tommy"));
         assertTrue(login.userInput.getString("anything").equals("password"));
-
     }
+
+    @Test
+    public void testThatInvalidUserMockReturnsBadNameThenBadPassword() throws Exception {
+        Login login = new Login(invalidMockUserInput);
+        assertTrue(login.userInput.getString("anything").equals("BadName"));
+        assertTrue(login.userInput.getString("anything").equals("BadPassword"));
+    }
+
+    @Test
+    public void testLoginWithValidUsernameReturnsTrue() throws Exception {
+        Login login = new Login(mockUserInput);
+        assertTrue(login.userExists("Tommy"));
+    }
+
+    @Test
+    public void testLoginWithInvalidUsernameReturnsFalse() throws Exception {
+        Login login = new Login(mockUserInput);
+        assertFalse(login.userExists("BadName"));
+    }
+
+    @Test
+    public void testLoginWithValidUserNameAndPassword() throws Exception {
+        Login login = new Login(mockUserInput);
+        assertTrue(login.correctPassword("Tommy", "password"));
+    }
+
+    @Test
+    public void testLoginWithValidUserNameAndInvalidPassword() throws Exception {
+        Login login = new Login(mockUserInput);
+        assertFalse(login.correctPassword("Tommy", "wrongPassword"));
+    }
+
+    @Test
+    public void testLoginWithInvalidUserNameAndInvalidPassword() throws Exception {
+        Login login = new Login(mockUserInput);
+        assertFalse(login.correctPassword("BadName", "wrongPassword"));
+    }
+
+
 }
